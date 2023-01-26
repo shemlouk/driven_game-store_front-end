e2d3c11df4420a0e3ae89990f35b96caef30dad8
samuel <shemlouk@gmail.com>
2023-01-25 21:37:52 -0300

    feat(header): add page Buttons to Header component

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,12 +1,22 @@
+import RoundButton from "../../components/RoundButton";
+import { Link } from "react-router-dom";
 import * as S from "./style";
 
 const Header = () => {
   return (
     <S.Container>
       <div>
-        <S.LogoFont color="#FFD60A">Game</S.LogoFont>
+        <S.LogoFont bColor="#FFD60A">Game</S.LogoFont>
         <S.LogoFont>Store</S.LogoFont>
       </div>
+      <S.ButtonsContainer>
+        <Link to="/library">
+          <RoundButton name="game" />
+        </Link>
+        <Link to="/checkout">
+          <RoundButton name="cart" />
+        </Link>
+      </S.ButtonsContainer>
     </S.Container>
   );
 };
--- src/layout/Header/style.js
+++ src/layout/Header/style.js
@@ -8,20 +8,26 @@ export const Container = styled.header`
   align-items: center;
   justify-content: space-between;
   background: linear-gradient(to bottom, #1b2838, transparent);
-  border-bottom: 1px solid #c7d5e020;
-  position: fixed;
   top: 0px;
   left: 0px;
+  position: fixed;
+  border-bottom: 1px solid #c7d5e020;
 `;
 
 export const LogoFont = styled.span`
-  font-family: "Press Start 2P", sans-serif !important;
   font-size: 35px;
-  background-color: ${({ color }) => (color ? color : "white")};
   color: transparent;
-  text-shadow: 0px 3px 1px rgba(0, 0, 0, 0.15);
-  -webkit-background-clip: text;
-  -moz-background-clip: text;
-  background-clip: text;
+  font-family: "Press Start 2P", sans-serif !important;
+  background-color: ${({ bColor }) => (bColor ? bColor : "white")};
   user-select: none;
+  background-clip: text;
+  -moz-background-clip: text;
+  -webkit-background-clip: text;
+  text-shadow: 0px 3px 1px rgba(0, 0, 0, 0.15);
+`;
+
+export const ButtonsContainer = styled.div`
+  width: 180px;
+  display: flex;
+  justify-content: space-between;
 `;
