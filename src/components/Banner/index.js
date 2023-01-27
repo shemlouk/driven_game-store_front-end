b5fd030b00ae8c8e7f0ffbf26d61f7e4f287dc78
samuel <shemlouk@gmail.com>
2023-01-27 13:36:30 -0300

    feat(banner): add title, price and button to Banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,9 +1,16 @@
 import * as S from "./style";
 
-const Banner = ({ image }) => {
+const Banner = ({ image, title, number }) => {
   return (
     <S.Container>
       <S.Image src={image} />
+      <S.Title>{title}</S.Title>
+      <S.Price>
+        {!number
+          ? "Free"
+          : "R$" + new Intl.NumberFormat("pt-BR").format(number)}
+      </S.Price>
+      <S.Button>Buy</S.Button>
     </S.Container>
   );
 };
--- src/components/Banner/style.js
+++ src/components/Banner/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
+  color: white;
   height: 660px;
   overflow: hidden;
   position: relative;
@@ -23,6 +24,52 @@ export const Image = styled.div`
     height: 100%;
     content: " ";
     position: absolute;
-    background: linear-gradient(to top, #000000 0%, transparent 50%);
+    background: linear-gradient(to top, #000000 0%, transparent 70%);
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 50px;
+  font-weight: 700;
+  position: absolute;
+  left: 70px;
+  bottom: 180px;
+  pointer-events: none;
+`;
+
+export const Price = styled.div`
+  width: 200px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: 70px;
+  bottom: 90px;
+  background: #c7d5e010;
+  font-size: 25px;
+  font-weight: 500;
+  border-top: 1px solid #c7d5e050;
+  border-radius: 50px;
+  backdrop-filter: blur(4px);
+  pointer-events: none;
+`;
+
+export const Button = styled.button`
+  width: 100px;
+  height: 60px;
+  border: none;
+  border-radius: 50px;
+  position: absolute;
+  left: 300px;
+  bottom: 90px;
+  font-size: 25px;
+  font-weight: 600;
+  color: white;
+  background: linear-gradient(to bottom, #66c0f4, #347faa);
+  transition: 0.3s;
+  cursor: pointer;
+  :hover {
+    box-shadow: 0 0 15px 7px #66c0f480;
   }
 `;
