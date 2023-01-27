ffba501b019396dcb40de3b2080a98c967fe8e5b
samuel <shemlouk@gmail.com>
2023-01-27 14:27:45 -0300

    style(layout): remove flex from Main component

--- src/layout/Main/index.js
+++ src/layout/Main/index.js
@@ -3,10 +3,6 @@ import styled from "styled-components";
 export const Main = styled.main`
   height: 100vh;
   padding-top: 100px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
 `;
 
 export default Main;
