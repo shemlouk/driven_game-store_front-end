0ba5a991f6dbe60763363a2ddaa7bb187923b1d1
samuel <shemlouk@gmail.com>
2023-01-27 01:13:54 -0300

    feat(layout): add Main styled component

---
+++ src/layout/Main/index.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  height: 100vh;
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Main;
