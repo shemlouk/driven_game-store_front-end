ae68f253281a14bad7197797dd43380c5939224b
samuel <shemlouk@gmail.com>
2023-01-27 22:38:00 -0300

    feat(pages): add products list to Home page

---
+++ src/pages/Home/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const SessionContainer = styled.div`
+  margin: 50px 70px 0px 70px;
+  p {
+    font-size: 30px;
+    font-weight: 700;
+    margin-bottom: 30px;
+    color: white;
+  }
+`;
+
+export const CardList = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
