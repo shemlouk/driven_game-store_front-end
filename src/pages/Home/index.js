7fffc3e57732a0791f5af12b7f9ceed9a78539e9
Davi-Mancebo <davimancebo@gmail.com>
2023-01-29 10:15:14 -0300

    add of login on a component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,16 +3,10 @@ import { Carousel } from "react-responsive-carousel";
 import Banner from "../../components/Banner";
 import Main from "../../layout/Main";
 import React, { useState } from "react";
-import { AuthContext } from "../../provider/provider";
-import * as S from "./style";
-import axios from "axios";
+
 
 const Home = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
-  const [img, setImg] = useState("");
-  const value = React.useContext(AuthContext);
+
   const [products, setProducts] = useState([
     {
       title: "Minecraft Legends",
@@ -36,99 +30,15 @@ const Home = () => {
     },
   ]);
 
-  function login() {
-    const dados = {
-      email: email,
-      password: password,
-    };
-    axios
-      .post(`${process.env.REACT_APP_API_BASE_URL}/login`, dados)
-      .then((crr) => {
-        console.log(crr.data);
-      })
-      .catch((err) => {
-        console.log(err.response);
-      });
-  }
-  function signUp() {
-    const dados = {
-      name: name,
-      email: email,
-      password: password,
-      img: img,
-    };
-    axios
-      .post(`${process.env.REACT_APP_API_BASE_URL}/sign-up`, dados)
-      .then((crr) => {
-        console.log(crr.data);
-      })
-      .catch((err) => {
-        console.log(err.response);
-      });
+
+
+  function clicked(e){
+    console.log(e.currentTarget)
   }
 
   return (
-    <Main>
-      {value.value && (
-        <S.Login>
-          <input
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            placeholder="Senha"
-            value={password}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button onClick={login}>Login</button>
-          <p
-            onClick={() => {
-              value.setValue(false);
-              value.setValue2(true);
-              setEmail("");
-              setPassword("");
-            }}
-          >
-            I don't have an accoount
-          </p>
-        </S.Login>
-      )}
-      {value.value2 && (
-        <S.SignUp>
-          <input
-            placeholder="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            placeholder="Senha"
-            value={password}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            placeholder="image (opc)"
-            value={img}
-            onChange={(e) => setImg(e.target.value)}
-          />
-          <button onClick={signUp}>Sign-up</button>
-          <p
-            onClick={() => {
-              value.setValue(true);
-              value.setValue2(false);
-            }}
-          >
-            I already have an accoount
-          </p>
-        </S.SignUp>
-      )}
+    <Main onClick={clicked}>
+
       <Carousel
         {...{
           infiniteLoop: true,
@@ -138,7 +48,6 @@ const Home = () => {
           autoPlay: true,
           interval: 5000,
         }}
-        onClick={() => value.setValue(false)}
       >
         {products.map((p, i) => (
           <Banner key={i} {...p} />
