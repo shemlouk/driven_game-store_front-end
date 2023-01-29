535694a89527df51f0d9007323387faae792c52a
samuel <shemlouk@gmail.com>
2023-01-29 15:55:01 -0300

    refactor(login): remove console.log

--- src/provider/provider.js
+++ src/provider/provider.js
@@ -1,28 +1,26 @@
 import React from "react";
 import { useState, useRef } from "react";
 
-export const AuthContext = React.createContext({})
+export const AuthContext = React.createContext({});
 
 export const AuthProvider = (props) => {
-    const [value, setValue] = useState(false)
-    const [value2, setValue2] = useState(false)
-    const loginRef = useRef(null)
+  const [value, setValue] = useState(false);
+  const [value2, setValue2] = useState(false);
+  const loginRef = useRef(null);
 
-    function clicked(e){
-        console.log(loginRef)
-        console.log(e.target)
-        if(e.target === loginRef.current){
-          console.log("correto")
-          return setValue(true)
-        }
-        console.log("errado")
-        setValue2(false)
-        return setValue(false)
-      }
-    
-    return (
-        <AuthContext.Provider value={{ value, setValue, value2, setValue2, clicked, loginRef }}>
-        {props.children}
-        </AuthContext.Provider>
-    );
-}
+  function clicked(e) {
+    if (e.target === loginRef.current) {
+      return setValue(true);
+    }
+    setValue2(false);
+    return setValue(false);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ value, setValue, value2, setValue2, clicked, loginRef }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
