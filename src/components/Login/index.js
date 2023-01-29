15da52789467f691147603d318b6dc6e178ea621
samuel <shemlouk@gmail.com>
2023-01-29 15:51:19 -0300

    feat(login): add inputs types

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,17 +4,19 @@ import * as S from "./style";
 import axios from "axios";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [img, setImg] = useState("");
   const value = useContext(AuthContext);
 
-  function login() {
+  function login(e) {
+    e.preventDefault();
     const dados = {
       email: email,
       password: password,
     };
+    console.log(dados);
     axios
       .post(`${process.env.REACT_APP_API_BASE_URL}/login`, dados)
       .then((crr) => {
@@ -24,13 +26,16 @@ export default function Login() {
         console.log(err.response);
       });
   }
-  function signUp() {
+
+  function signUp(e) {
+    e.preventDefault();
     const dados = {
       name: name,
       email: email,
       password: password,
-      img: img,
+      img: img ? img : "http://test",
     };
+    console.log(dados);
     axios
       .post(`${process.env.REACT_APP_API_BASE_URL}/sign-up`, dados)
       .then((crr) => {
@@ -40,22 +45,26 @@ export default function Login() {
         console.log(err.response);
       });
   }
+
   return (
     <>
       {value.value && (
-        <S.Login>
+        <S.Login onSubmit={login}>
           <input
             placeholder="Email"
             value={email}
+            type="email"
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
           <input
             placeholder="Senha"
             value={password}
             type="password"
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
-          <button onClick={login}>Login</button>
+          <button>Entrar</button>
           <p
             onClick={() => {
               value.setValue(false);
@@ -64,34 +73,40 @@ export default function Login() {
               setPassword("");
             }}
           >
-            I don't have an accoount
+            Eu não tenho uma conta
           </p>
         </S.Login>
       )}
+
       {value.value2 && (
-        <S.SignUp>
+        <S.SignUp onSubmit={signUp}>
           <input
-            placeholder="name"
+            placeholder="Nome"
             value={name}
-            onChange={(e) => setName(e.target.vaölue)}
+            onChange={(e) => setName(e.target.value)}
+            required
           />
           <input
             placeholder="Email"
             value={email}
+            type="email"
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
           <input
             placeholder="Senha"
             value={password}
             type="password"
             onChange={(e) => setPassword(e.target.value)}
+            required
           />
           <input
-            placeholder="image (opc)"
+            placeholder="Imagem"
             value={img}
+            type="url"
             onChange={(e) => setImg(e.target.value)}
           />
-          <button onClick={signUp}>Sign-up</button>
+          <button>Cadastrar</button>
           <p
             onClick={() => {
               value.setValue(true);
@@ -100,7 +115,7 @@ export default function Login() {
               setPassword("");
             }}
           >
-            I already have an accoount
+            Eu já tenho uma conta
           </p>
         </S.SignUp>
       )}
